e7b2946bfcdd4d7a3026744ad68268218a820331
emilien <emilien.charbonnier@cegetel.net>
2012-04-07 09:22:39 +0200

    home page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newdiet::Application.config.secret_token = '84f63df52889acccd847c8fd5b7996f4a70d020a6d61fe25791715dde83017c757967871a34225feed69913472918b4d957eff3eb15861e063ef7f8a8620bcd6'
