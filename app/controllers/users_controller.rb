975ec0a8383fe30134b067351b1ccd4c759101a8
emilien <emilien.charbonnier@cegetel.net>
2012-04-08 11:58:48 +0200

    prolfil update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
   
+  def edit
+    @user = current_user   
+  end
+  
+  def update
+     @user = User.find(params[:id])
+     if @user.update_attributes(params[:user])
+       flash[:success] = "Profil mis a jour"
+       redirect_to @user
+     else
+       render 'edit'
+     end
+   end
+  
   def show
     @user = current_user
   end
